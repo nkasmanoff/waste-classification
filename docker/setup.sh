f213dbb22441c3d0ce2a733d55d0c0971983934d
nkasmanoff <nkasmanoff@gmail.com>
2021-09-27 12:41:01 -0400

    fix path

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,3 +1,3 @@
 # Instantiates the jetson-inference docker container which provides all of the necessary dependencies and command line arguments.
 cd ../jetson-inference
-docker/run.sh --volume /home/noah/garbage_classification:/garbage_classification  # replace with your name
+docker/run.sh --volume /home/noah/waste-classification:/waste-classification  # replace with your name
