53a351ebb17e61b5f750fa0d429a879c13f59b09
nkasmanoff <nkasmanoff@gmail.com>
2021-10-04 16:05:16 -0400

    fully automate setup

--- docker/setup.sh
+++ docker/setup.sh
@@ -1,3 +1,3 @@
 # Instantiates the jetson-inference docker container which provides all of the necessary dependencies and command line arguments.
 cd ../jetson-inference
-docker/run.sh --volume /home/noah/waste-classification:/waste-classification  # replace with your name
+docker/run.sh --v /home/noah/waste-classification:/waste-classification  -c "cd /waste-classification && pip install wandb"
